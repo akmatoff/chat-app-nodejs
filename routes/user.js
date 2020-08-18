77e3734fb3aefcff9e74f614e7a84f26c3e92c86
unknown <azim.akmatov1@gmail.com>
2020-08-18 16:33:49 +0600

    Send tokens after authorization

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-router.post('/signup', (req, res) => {
+// Signup route
+router.post('/signup', (req, res, next) => {
     User.findOne({ where: { username: req.body.username } }).then(user => {
         if (user != null) {
             return res.status(409).json({
@@ -37,6 +39,48 @@ router.post('/signup', (req, res) => {
             });
         }
     })
+});
+
+// Login route
+router.post('/login', (req, res) => {
+    User.findOne({ where: { username: req.body.username } })
+        .then(user => {
+            if (user === null) {
+                return res.status(404).json({
+                    message: "User not found"
+                })
+            }
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Authorization failed'
+                    });
+                }
+
+                if (result) {
+                    const token = jwt.sign({
+                        userID: user.user_id,
+                        username: user.username
+                    }, process.env.JWT_KEY, {
+                        expiresIn: "1h"
+                    })
+                    return res.status(200).json({
+                        message: 'Successful authorization',
+                        token: token
+                    });
+                } else {
+                    return res.status(401).json({
+                        message: 'Authorization failed'
+                    });
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
-
+const dotenv = require('dotenv');
 const PORT = process.env.PORT || 3000;
 
+dotenv.config();
 server.listen(PORT, console.log(`Server running on ${PORT}`));
