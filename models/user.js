5800a9b46e8edc33f49ccb480dc6ee54a136b533
unknown <azim.akmatov1@gmail.com>
2020-08-18 15:22:55 +0600

    Backend user authorization finished

--- models/user.js
+++ models/user.js
@@ -1,21 +1,23 @@
 const { DataTypes } = require('sequelize');
 const db = require('../config/db');
 
-// User model
 const User = db.define('User', {
-    user_id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-});
+  user_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+})
+
+User.sync();
 
 module.exports = User;
--- routes/user.js
+++ routes/user.js
@@ -4,19 +4,17 @@ const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
-// POST REQUEST TO SIGNUP ROUTE
 router.post('/signup', (req, res) => {
     User.findOne({ where: { username: req.body.username } }).then(user => {
-        // If user found
-        if (user === null) {
+        if (user != null) {
             return res.status(409).json({
-                message: "User already exists"
+                error: "User already exists"
             });
         } else {
             bcrypt.hash(req.body.password, 10, (err, hash) => {
                 if (err) {
                     return res.status(500).json({
-                        error: err
+                        error: err.message
                     });
                 } else {
                     const user = User.build({
@@ -26,17 +24,17 @@ router.post('/signup', (req, res) => {
                     user.save().then(result => {
                         console.log(result);
                         res.status(201).json({
-                            message: "User created"
+                            message: 'User created'
                         });
                     })
                         .catch(err => {
                             console.log(err);
                             res.status(500).json({
-                                error: err
-                            });
-                        });
+                                error: err.message
+                            })
+                        })
                 }
-            })
+            });
         }
     })
 });
